0e165dc8467b627763d256cdf8fc94ecf082edd2
alexrossetto <alexandredrossetto@hotmail.com>
2022-10-20 04:42:45 -0300

    integrating project to githubs OAuth authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,30 @@
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 
 export function SignInButton() {
 
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
+  console.log(session, "user session")
+
+  return session ? (
     <button 
       className={styles.signInButton} 
       type="button"
+      onClick={() => signOut()}
       >
         <FaGithub color="#04d361"/>
-        Alexandre Rossetto
+          {session.user.name}
         <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
       className={styles.signInButton} 
       type="button"
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sign in with GitHub
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,21 @@
 import { AppProps } from 'next/app'
+import { Session } from "next-auth"
 import { Header } from '../components/Header'
+import { SessionProvider as NextAuthProvider } from 'next-auth/react';
 
 import '../styles/global.scss'
 
 //Tudo que está aqui dentro é renderizado / recarregado sempre que o usuario navega entre rotas.
 
-function MyApp({ Component, pageProps } : AppProps) {
+function MyApp(
+  { Component, pageProps } : AppProps<{
+    session: Session;
+  }>) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
